c3b362f64fd7438dac66d173c32ed2b4e0a87939
Madhav Prajapati <madhavku248@gmail.com>
2024-07-11 16:47:16 +0530

    testing

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! madhav'
+    return 'Hello, World! madhav prajapati'
 
 if __name__ == '__main__':
     app.run(debug=True)
